dedc9b95376f65b5d62b487d10c1189fc9ca8c58
francosansone <fracosansone11@gmail.com>
2018-10-09 20:31:16 -0300

    array size fixed

--- code/test/cat.c
+++ code/test/cat.c
@@ -5,7 +5,7 @@
 
 int main(int argc, char **argv){
   for(unsigned i = 0; i < argc; i++){
-    char *buff;               //Max size of file: 128
+    char buff[128];               //Max size of file: 128
     OpenFileId o = Open(argv[i]); //Open the file
     for(unsigned j = 0; j < 128 && Read(buff + j, 1,o) > 0; j++)
       Write(buff + j, 1, OUTPUT);
