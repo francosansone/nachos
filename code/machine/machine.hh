0318252d1377b1b50824f22ae0226733cec177e8
francosansone <fracosansone11@gmail.com>
2019-01-07 20:37:26 -0300

    memory size for vmem purposes

--- code/machine/machine.hh
+++ code/machine/machine.hh
@@ -31,9 +31,9 @@
 const unsigned PAGE_SIZE = SECTOR_SIZE;  ///< Set the page size equal to the
                                          ///< disk sector size, for
                                          ///< simplicity.
-const unsigned NUM_PHYS_PAGES = 256;
+const unsigned NUM_PHYS_PAGES = 128;
 const unsigned MEMORY_SIZE = NUM_PHYS_PAGES * PAGE_SIZE;
-const unsigned TLB_SIZE = 32;  ///< if there is a TLB, make it small.
+const unsigned TLB_SIZE = 8;  ///< if there is a TLB, make it small.
 
 enum ExceptionType {
     NO_EXCEPTION = 0,             // Everything ok!
